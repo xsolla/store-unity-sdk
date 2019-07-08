cae1cde8c280b56c7c74bbf1b72cd701a0aee9f9
ivan_tustanivsky <itustanivsky@ninevastudios.com>
2019-07-08 17:24:38 +0300

    add settings editor placeholder

---
+++ Assets/Xsolla/Scripts/Core/Settings/XsollaSettings.cs
@@ -0,0 +1,146 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Xsolla.Core
+{
+	public class XsollaSettings : ScriptableObject
+	{
+		const string SettingsAssetName = "XsollaSettings";
+		const string SettingsAssetPath = "Resources/";
+		const string SettingsAssetExtension = ".asset";
+
+		static XsollaSettings _instance;
+
+		[SerializeField]
+		string loginId;
+		[SerializeField]
+		bool useJwtValidation;
+		[SerializeField]
+		string jwtValidationUrl;
+		[SerializeField]
+		bool useProxy;
+		[SerializeField]
+		string callbackUrl;
+		
+		public static string LoginId
+		{
+			get { return Instance.loginId; }
+			set
+			{
+				Instance.loginId = value;
+				MarkAssetDirty();
+			}
+		}
+
+		public static bool UseJwtValidation
+		{
+			get { return Instance.useJwtValidation; }
+			set
+			{
+				Instance.useJwtValidation = value;
+				MarkAssetDirty();
+			}
+		}
+
+		public static string JwtValidationUrl
+		{
+			get { return Instance.jwtValidationUrl; }
+			set
+			{
+				Instance.jwtValidationUrl = value;
+				MarkAssetDirty();
+			}
+		}
+
+		public static bool UseProxy
+		{
+			get { return Instance.useProxy; }
+			set
+			{
+				Instance.useProxy = value;
+				MarkAssetDirty();
+			}
+		}
+
+		public static string CallbackUrl
+		{
+			get { return Instance.callbackUrl; }
+			set
+			{
+				Instance.callbackUrl = value;
+				MarkAssetDirty();
+			}
+		}
+
+		public static XsollaSettings Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = Resources.Load(SettingsAssetName) as XsollaSettings;
+					if (_instance == null)
+					{
+						_instance = CreateInstance<XsollaSettings>();
+						SaveAsset(Path.Combine(GetSdkPath(), SettingsAssetPath), SettingsAssetName);
+					}
+				}
+
+				return _instance;
+			}
+		}
+		
+		public static string GetSdkPath()
+		{
+			return GetAbsoluteSdkPath().Replace("\\", "/").Replace(Application.dataPath, "Assets");
+		}
+
+		static string GetAbsoluteSdkPath()
+		{
+			return Path.GetDirectoryName(Path.GetDirectoryName(FindEditor(Application.dataPath)));
+		}
+
+		static string FindEditor(string path)
+		{
+			foreach (var d in Directory.GetDirectories(path))
+			{
+				foreach (var f in Directory.GetFiles(d))
+				{
+					if (f.Contains("XsollaSettingsEditor.cs"))
+					{ 
+						return f;
+					}
+				}
+
+				var rec = FindEditor(d);
+				if (rec != null)
+				{
+					return rec;
+				}
+			}
+
+			return null;
+		}
+
+		static void SaveAsset(string directory, string name)
+		{
+#if UNITY_EDITOR
+			if (!Directory.Exists(directory))
+			{
+				Directory.CreateDirectory(directory);
+			}
+
+			AssetDatabase.CreateAsset(Instance, directory + name + SettingsAssetExtension);
+			AssetDatabase.Refresh();
+#endif
+		}
+
+		static void MarkAssetDirty()
+		{
+#if UNITY_EDITOR
+			EditorUtility.SetDirty(Instance);
+#endif
+		}
+	}
+}
---
+++ Assets/Xsolla/Scripts/Core/Settings/XsollaSettingsEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Xsolla.Core
+{
+	[CustomEditor(typeof(XsollaSettings))]
+	public class XsollaSettingsEditor : UnityEditor.Editor
+	{
+		const string LoginIdTooltip = "Login ID from your Publisher Account.";
+		const string JwtValidationUrlTooltip = "Server URL to validate the JWT on your side. Required if the JWT validation box is ticked.";
+		const string CallbackUrlTooltip = "URL to redirect the user to after registration/authentication/password reset. " +
+		                                  "Must be identical to Callback URL specified in Publisher Account in Login settings. Required if there are several Callback URLs.";
+		
+		[MenuItem("Window/Xsolla/Edit Settings", false, 1000)]
+		public static void Edit()
+		{
+			Selection.activeObject = XsollaSettings.Instance;
+		}
+
+		public override void OnInspectorGUI()
+		{
+			using (new EditorGUILayout.VerticalScope("box"))
+			{
+				GUILayout.Label("Login SDK Settings", EditorStyles.boldLabel);
+				
+				XsollaSettings.LoginId = EditorGUILayout.TextField(new GUIContent("Login ID [?]", LoginIdTooltip),  XsollaSettings.LoginId);
+				XsollaSettings.UseJwtValidation = EditorGUILayout.Toggle("Enable JWT token validation?", XsollaSettings.UseJwtValidation);
+				XsollaSettings.JwtValidationUrl = EditorGUILayout.TextField(new GUIContent("JWT validation URL [?]", JwtValidationUrlTooltip),  XsollaSettings.JwtValidationUrl);
+				XsollaSettings.UseProxy = EditorGUILayout.Toggle("Enable proxy?", XsollaSettings.UseProxy);
+				XsollaSettings.CallbackUrl = EditorGUILayout.TextField(new GUIContent("Callback URL [?]", CallbackUrlTooltip),  XsollaSettings.CallbackUrl);
+			}
+      
+			EditorGUILayout.Space();
+		}
+	}
+}
+
