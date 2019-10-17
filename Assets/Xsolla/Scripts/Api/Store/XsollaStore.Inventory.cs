b88092758c7c75f969d495a7438d48f23d00c3d4
Levin Maxim <m.levin@xsolla.com>
2019-10-17 16:08:44 +0500

    add virtual currency methods to api
    - add virtual currency balance entity
    - fix StoreItem format
    - split XsollaStore methods to partial files

---
+++ Assets/Xsolla/Scripts/Api/Store/XsollaStore.Inventory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using UnityEngine;
+using Xsolla.Core;
+
+namespace Xsolla.Store
+{
+	public partial class XsollaStore : MonoSingleton<XsollaStore>
+	{
+		private const string URL_INVENTORY_GET_ITEMS = "https://store.xsolla.com/api/v1/project/{0}/user/inventory/items";
+		private const string URL_INVENTORY_ITEM_CONSUME = "https://store.xsolla.com/api/v1/project/{0}/user/inventory/item/consume";
+
+		public void GetInventoryItems(string projectId, [NotNull] Action<InventoryItems> onSuccess, [CanBeNull] Action<Error> onError, [CanBeNull] string locale = null)
+		{
+			var urlBuilder = new StringBuilder(string.Format(URL_INVENTORY_GET_ITEMS, projectId)).Append(AdditionalUrlParams);
+			urlBuilder.Append(GetLocaleUrlParam(locale));
+
+			WebRequestHelper.Instance.GetRequest(urlBuilder.ToString(), WebRequestHeader.AuthHeader(Token), onSuccess, onError, Error.ItemsListErrors);
+		}
+
+		public void ConsumeInventoryItem(string projectId, ConsumeItem item, [CanBeNull] Action onSuccess, [CanBeNull] Action<Error> onError)
+		{
+			var urlBuilder = new StringBuilder(string.Format(URL_INVENTORY_ITEM_CONSUME, projectId)).Append(AdditionalUrlParams);
+
+			var headers = new List<WebRequestHeader>() { WebRequestHeader.AuthHeader(Token), WebRequestHeader.ContentTypeHeader() };
+
+			WebRequestHelper.Instance.PostRequest(urlBuilder.ToString(), item.ToJson(), null, headers, onSuccess, onError);
+		}
+	}
+}
---
+++ Assets/Xsolla/Scripts/Api/Store/XsollaStore.VirtualCurrency.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using UnityEngine;
+using Xsolla.Core;
+
+namespace Xsolla.Store
+{
+	public partial class XsollaStore : MonoSingleton<XsollaStore>
+	{
+		private const string URL_VIRTUAL_CURRENCY_BALANCE = "https://store.xsolla.com/api/v2/project/{0}/user/virtual_currency_balance";
+		private const string URL_VIRTUAL_CURRENCY_LIST = "https://store.xsolla.com/api/v2/project/{0}/items/virtual_currency";
+		private const string URL_VIRTUAL_CURRENCY_PACKAGES_IN_PROJECT = "https://store.xsolla.com/api/v2/project/{0}/items/virtual_currency/package";
+
+		public void GetVirtualCurrencyBalance(string projectId, [NotNull] Action<UserVirtualCurrenciesBalance> onSuccess, [CanBeNull] Action<Error> onError, [CanBeNull] string locale = null)
+		{
+			var urlBuilder = new StringBuilder(string.Format(URL_VIRTUAL_CURRENCY_BALANCE, projectId)).Append(AdditionalUrlParams);
+			urlBuilder.Append(GetLocaleUrlParam(locale));
+
+			WebRequestHelper.Instance.GetRequest(urlBuilder.ToString(), WebRequestHeader.AuthHeader(Token), onSuccess, onError, Error.ItemsListErrors);
+		}
+
+		public void GetVirtualCurrencyList(string projectId, [NotNull] Action<StoreItems> onSuccess, [CanBeNull] Action<Error> onError, [CanBeNull] string locale = null)
+		{
+			var urlBuilder = new StringBuilder(string.Format(URL_VIRTUAL_CURRENCY_LIST, projectId)).Append(AdditionalUrlParams);
+			urlBuilder.Append(GetLocaleUrlParam(locale));
+
+			WebRequestHelper.Instance.GetRequest(urlBuilder.ToString(), WebRequestHeader.AuthHeader(Token), onSuccess, onError, Error.ItemsListErrors);
+		}
+
+		// TODO: coming soon.
+		//private const string URL_VIRTUAL_CURRENCY_INFO = "https://store.xsolla.com/api/v2/project/{0}/items/virtual_currency/sku/{1}";
+		//public void GetVirtualCurrencyInfo(string projectId, string sku, [NotNull] Action<InventoryItems> onSuccess, [CanBeNull] Action<Error> onError, [CanBeNull] string locale = null)
+		//{
+		//	var urlBuilder = new StringBuilder(string.Format(URL_VIRTUAL_CURRENCY_INFO, projectId, sku)).Append(AdditionalUrlParams);
+		//	urlBuilder.Append(GetLocaleUrlParam(locale));
+
+		//	WebRequestHelper.Instance.GetRequest(urlBuilder.ToString(), WebRequestHeader.AuthHeader(Token), onSuccess, onError, Error.ItemsListErrors);
+		//}
+
+		public void GetVirtualCurrencyPackagesList(string projectId, [NotNull] Action<StoreItems> onSuccess, [CanBeNull] Action<Error> onError, [CanBeNull] string locale = null)
+		{
+			var urlBuilder = new StringBuilder(string.Format(URL_VIRTUAL_CURRENCY_PACKAGES_IN_PROJECT, projectId)).Append(AdditionalUrlParams);
+			urlBuilder.Append(GetLocaleUrlParam(locale));
+
+			WebRequestHelper.Instance.GetRequest(urlBuilder.ToString(), WebRequestHeader.AuthHeader(Token), onSuccess, onError, Error.ItemsListErrors);
+		}
+
+		// TODO: coming soon.
+		//private const string URL_VIRTUAL_CURRENCY_PACKAGES = "https://store.xsolla.com/api/v2/project/{0}/items/virtual_currency/package/sku/{1}";
+		//public void GetVirtualCurrencyPackagesBySku(string projectId, string sku, [NotNull] Action<InventoryItems> onSuccess, [CanBeNull] Action<Error> onError, [CanBeNull] string locale = null)
+		//{
+		//	var urlBuilder = new StringBuilder(string.Format(URL_VIRTUAL_CURRENCY_PACKAGES, projectId, sku)).Append(AdditionalUrlParams);
+		//	urlBuilder.Append(GetLocaleUrlParam(locale));
+
+		//	WebRequestHelper.Instance.GetRequest(urlBuilder.ToString(), WebRequestHeader.AuthHeader(Token), onSuccess, onError, Error.ItemsListErrors);
+		//}
+	}
+}
