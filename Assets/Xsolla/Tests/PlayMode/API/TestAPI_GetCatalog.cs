d6288a8ee075506cf1fa574e3aa48f54770ef0f1
m.pystogov <MAXxsolla23>
2019-12-16 16:33:48 +0500

    Change ProjectID = 50458

--- Assets/Xsolla/Tests/PlayMode/API/TestAPI_GetCatalog.cs
+++ Assets/Xsolla/Tests/PlayMode/API/TestAPI_GetCatalog.cs
@@ -12,39 +12,61 @@ namespace Tests
 {
     public class TestAPI_GetCatalog : BaseTestApiScript
     {
-		protected override void Request()
-		{
-			StoreAPI.GetListOfItems(XsollaSettings.StoreProjectId, SuccessRequest, FailedRequest);
-		}
+        const int CATALOG_SIZE = 1;
 
-		private void SuccessRequest(StoreItems items)
-		{
-			CheckItems(items);
-			Complete();
-		}
+        protected override void Request()
+        {
+            StoreAPI.GetListOfItems(XsollaSettings.StoreProjectId, SuccessRequest, FailedRequest);
+        }
 
-		private void FailedRequest(Error error)
-		{
-			Assert.Fail(error.errorMessage);
-			Complete();
-		}
+        private void SuccessRequest(StoreItems items)
+        {
+            try
+            {
+                CheckItems(items);
+            } catch(Exception e)
+            {
+                Complete();
+                throw e;
+            }            
+        }
 
-		private void CheckItems(StoreItems items)
-		{
-			foreach(StoreItem item in items.items.ToList()) {
-				if (!CheckItem(item, out string message)) {
-					Assert.Fail(message);
-				}
-			}
-		}
+        private void FailedRequest(Error error) 
+        {
+            Assert.Fail(error.errorMessage);
+            Complete();
+        }
 
-		private bool CheckItem(StoreItem item, out string message)
-		{
-			message = "no message";
+        private void CheckItems(StoreItems items)
+        {
+            CheckCatalogSize(items);
 
-			Assert.False(string.IsNullOrEmpty(item.sku), "Sku is null or empty");
+            foreach (StoreItem item in items.items.ToList())
+            {
+                if (!CheckItem(item, out string message))
+                {
+                    Assert.Fail(message);
+                }
+            }
+        }
 
-			return true;
-		}
-	}
+        private void CheckCatalogSize(StoreItems items)
+        {
+            int catalogSize = items.items.Count();
+            bool condition = catalogSize == CATALOG_SIZE;
+            Assert.True(condition,
+                "Catalog size must be = " + CATALOG_SIZE +
+                " but we have = " + catalogSize
+                );
+        }
+
+        private bool CheckItem(StoreItem item, out string message)
+        {
+            message = "no message";
+
+            Assert.False(string.IsNullOrEmpty(item.sku), "Sku is null or empty");
+
+            return true;
+        }
+    }
 }
--- Assets/Xsolla/Tests/PlayMode/XsollaSettings/Test_XsollaSettings.cs
+++ Assets/Xsolla/Tests/PlayMode/XsollaSettings/Test_XsollaSettings.cs
@@ -1,15 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework;
-using UnityEngine;
-using UnityEngine.TestTools;
+﻿using NUnit.Framework;
 using Xsolla.Core;
 
 namespace Tests
 {
     public class Test_XsollaSettings
     {
-		const string XSOLLA_PROJECT_ID_FOR_TESTS = "49999";
+		const string XSOLLA_PROJECT_ID_FOR_TESTS = "50458";
         
         [Test]
         public void Test_XsollaSettingsSimplePasses()
