920feea3cefaa6ade75f1e6cfee2d35ffc2e1984
max <m.pystogov@xsolla.com>
2020-03-12 17:08:48 +0500

    Add AutorizationTest

---
+++ Assets/UI_tests/Editor/AutorizationTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+public class AutorizationTest
+{
+    public AltUnityDriver AltUnityDriver;
+    //Before any test it connects with the socket
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        AltUnityDriver =new AltUnityDriver();
+    }
+
+    //At the end of the test closes the connection with the socket
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        AltUnityDriver.Stop();
+    }
+
+    [Test]
+    public void Test()
+    {
+
+        AltUnityDriver.LoadScene("Login");
+        AltUnityObject login = AltUnityDriver.FindElement("LoginUsername");
+        login.ClickEvent();
+        login.SetText("test123");
+        AltUnityObject pass = AltUnityDriver.FindElement("LoginPassword");
+        pass.ClickEvent();
+        pass.SetText("232323");
+        AltUnityDriver.FindElement("LoginButton").ClickEvent();
+        AltUnityDriver.LoadScene("Store");
+
+    }
+
+}
