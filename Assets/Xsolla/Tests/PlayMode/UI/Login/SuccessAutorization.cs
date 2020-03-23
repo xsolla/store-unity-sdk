4da82bbc08adc214472937efe7485a2e88dbf59d
max <m.pystogov@xsolla.com>
2020-03-23 17:01:37 +0500

    add SuccessAutorization test

--- Assets/Xsolla/Tests/PlayMode/UI/Login/SuccessAutorization.cs
+++ Assets/Xsolla/Tests/PlayMode/UI/Login/SuccessAutorization.cs
@@ -1,31 +1,43 @@
 ﻿using System;
+using System.Collections;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
 using Xsolla.Core;
 using Xsolla.Store;
 
 namespace Tests
-
 {
-
     public class SuccessAutorization
     {
-
         const int LOGIN = 123;
 
-
-        private void SuccessLogin()
+        [UnityTest]
+        public IEnumerator AuthTest()
         {
-            try
+            Debug.Log("Load scene");
+            SceneManager.LoadScene("Login");
+            yield return new WaitWhile(() =>
+                SceneManager.GetActiveScene() != SceneManager.GetSceneByName("Login")
+            );
+            Debug.Log("Wait 5 sec");
+            yield return new WaitForSeconds(5.0F);
 
-            { }
-
-            finally
-            {
-                Complete();
-            }
+            Debug.Log("Find login");
+            yield return new WaitWhile(() =>
+                GameObject.Find("Login") == null
+            );
+            Debug.Log("Wait 5 sec");
+            yield return new WaitForSeconds(5.0F);
+            GameObject login = GameObject.Find("Login");
+            Debug.Log("Set text");
+            InputField inputField = login.GetComponent<InputField>();
+            inputField.ActivateInputField();
+            inputField.textComponent.text = "Text";
+            Debug.Log("Wait 5 sec");
+            yield return new WaitForSeconds(5.0F);
         }
-
-            
-
-        
     }
 }
