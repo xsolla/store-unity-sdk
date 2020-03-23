2968d2546fc91e1ba0b37cb9bae28ad2c9d8d3a1
max <m.pystogov@xsolla.com>
2020-03-23 16:22:09 +0500

    Add scripts

---
+++ Assets/Xsolla/Tests/PlayMode/UI/Login/SuccessAutorization.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xsolla.Core;
+using Xsolla.Store;
+
+namespace Tests
+
+{
+
+    public class SuccessAutorization
+    {
+
+        const int LOGIN = 123;
+
+
+        private void SuccessLogin()
+        {
+            try
+
+            { }
+
+            finally
+            {
+                Complete();
+            }
+        }
+
+            
+
+        
+    }
+}
