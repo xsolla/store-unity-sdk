f0285cef2d69a52799f07825f541586e997cc0f7
max <m.pystogov@xsolla.com>
2020-03-24 12:44:51 +0500

    temporary commit

--- Assets/Xsolla/Tests/PlayMode/UI/Login/FailedAutorization.cs
+++ Assets/Xsolla/Tests/PlayMode/UI/Login/FailedAutorization.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+using Xsolla.Core;
+using Xsolla.Store;
+using NUnit.Framework;
 
-public class NewBehaviourScript : MonoBehaviour
+namespace Tests
 {
-    // Start is called before the first frame update
-    void Start()
+    public class FailedAutorization
     {
-        
-    }
+        const string USERNAME_FIELD = "LoginUsername";
+        const string USERPASSWORD_FIELD = "LoginPassword";
+        const string USERLOGIN_BUTTON = "LoginButton";
+        const string POPUP_ERROR = "ErrorPopUp";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [UnityTest]
+
+     
+        public IEnumerator FailedAutorizationTest()
+        {
+            SceneManager.LoadScene("Login");
+            yield return new WaitWhile(() =>
+                SceneManager.GetActiveScene() != SceneManager.GetSceneByName("Login")
+            );
+            yield return new WaitWhile(() =>
+                GameObject.Find(USERNAME_FIELD) == null
+            );
+            GameObject.Find(USERNAME_FIELD).GetComponent<InputField>().text = "test123";
+            GameObject.Find(USERPASSWORD_FIELD).GetComponent<InputField>().text = "232324";
+            yield return new WaitForSeconds(0.01F);
+            GameObject.Find(USERLOGIN_BUTTON).GetComponent<Button>().onClick.Invoke();
+            yield return new WaitForSeconds(4.0F);
+            Assert.True(GameObject.Find(POPUP_ERROR));
+
+        }
     }
 }
--- Assets/Xsolla/Tests/PlayMode/UI/Login/SuccessAutorization.cs
+++ Assets/Xsolla/Tests/PlayMode/UI/Login/SuccessAutorization.cs
@@ -7,18 +7,19 @@ using UnityEngine.TestTools;
 using UnityEngine.UI;
 using Xsolla.Core;
 using Xsolla.Store;
+using NUnit.Framework;
 
 namespace Tests
 {
     public class SuccessAutorization
     {
-        const int LOGIN = 123;
-
         const string USERNAME_FIELD = "LoginUsername";
-
+        const string USERPASSWORD_FIELD = "LoginPassword";
+        const string USERLOGIN_BUTTON = "LoginButton";
 
         [UnityTest]
-        public IEnumerator AuthTest()
+
+        public IEnumerator SuccessAutorizationTest()
         {
             SceneManager.LoadScene("Login");
             yield return new WaitWhile(() =>
@@ -28,9 +29,12 @@ namespace Tests
                 GameObject.Find(USERNAME_FIELD) == null
             );
             GameObject.Find(USERNAME_FIELD).GetComponent<InputField>().text = "test123";
-            // GameObject.Find(USERNAME_FIELD).GetComponent<Button>().SendMessage()
-
-            yield return new WaitForSeconds(5.0F);
+            GameObject.Find(USERPASSWORD_FIELD).GetComponent<InputField>().text = "232323";
+            yield return new WaitForSeconds(0.01F);
+            GameObject.Find(USERLOGIN_BUTTON).GetComponent<Button>().onClick.Invoke();
+            yield return new WaitForSeconds(4.0F);
+            Assert.True(SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Store"));
+            
         }
     }
 }
