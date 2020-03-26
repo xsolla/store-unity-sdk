b129a338a86741b79df3728238faaca6ee698197
max <m.pystogov@xsolla.com>
2020-03-26 17:53:31 +0500

    Add 3 new autotest

--- Assets/Xsolla/Tests/PlayMode/UI/Login/Autorization.cs
+++ Assets/Xsolla/Tests/PlayMode/UI/Login/Autorization.cs
@@ -20,32 +20,36 @@ namespace Tests
         const string POPUP_ERROR = "ErrorPopUp";
 
         [UnityTest]
-        public IEnumerator SuccessAutorizationTest()
+        public IEnumerator SuccessAutorization()
         {
-            Debug.Log("SUCCESS CASE");
+            
             TestHelper helper = TestHelper.Instance;
             yield return helper.LoadScene(TestHelper.Scenes.Login);
+            yield return helper.WaitFor(1.0F);
             helper.SetInputField(USERNAME_FIELD, "test123");
             helper.SetInputField(USERPASSWORD_FIELD, "232323");
             yield return helper.WaitFor(0.01F);
             helper.ClickButton(USERLOGIN_BUTTON);
-            yield return helper.WaitFor(8.0F);
+            yield return helper.WaitFor(5.0F);
             Assert.True(helper.IsScene(TestHelper.Scenes.Store));
+            yield return helper.WaitFor(1.5F);
         }
 
         [UnityTest]
-        public IEnumerator FailedAutorizationTest()
+        public IEnumerator FailedAutorization()
         {
-            Debug.Log("FAILED CASE");
+     
             TestHelper helper = TestHelper.Instance;
             yield return helper.LoadScene(TestHelper.Scenes.Login);
+            yield return helper.WaitFor(1.0F);
             helper.SetInputField(USERNAME_FIELD, "test123");
             helper.SetInputField(USERPASSWORD_FIELD, "232324");
             yield return helper.WaitFor(0.01F);
             helper.ClickButton(USERLOGIN_BUTTON);
-            yield return helper.WaitFor(4.0F);
+            yield return helper.WaitFor(1.5F);
             Assert.True(GameObject.Find(POPUP_ERROR));
             Assert.True(helper.IsScene(TestHelper.Scenes.Login));
+            yield return helper.WaitFor(1.0F);
         }
     }
 }
--- Assets/Xsolla/Tests/PlayMode/UI/Login/ResetPassword.cs
+++ Assets/Xsolla/Tests/PlayMode/UI/Login/ResetPassword.cs
@@ -1,18 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+using Xsolla.Core;
+using Xsolla.Store;
+using NUnit.Framework;
 
-public class ResetPassword : MonoBehaviour
+namespace Tests
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ResetPassword
     {
-        
-    }
+        const string RESET_URL = "ResetPasswordButton";
+        const string RESET_FIELD = "ResetPasswordEmail";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        const string POPUP_SUCCESS = "ExtendedPopUp";
+
+        [UnityTest]
+        public IEnumerator SuccessResetPassword()
+        {
+
+            TestHelper helper = TestHelper.Instance;
+            yield return helper.LoadScene(TestHelper.Scenes.Login);
+            yield return helper.WaitFor(1.4F);
+            helper.ClickButton(RESET_URL);
+            yield return helper.WaitFor(0.6F);
+            helper.SetInputField(RESET_FIELD, "TestReset@mail.ru");
+            yield return helper.WaitFor(0.6F);
+            helper.ClickButton(RESET_URL);
+            yield return helper.WaitFor(1.0F);
+            Assert.True(GameObject.Find(POPUP_SUCCESS));
+            Assert.True(helper.IsScene(TestHelper.Scenes.Login));
+        }
+
+    
     }
 }
