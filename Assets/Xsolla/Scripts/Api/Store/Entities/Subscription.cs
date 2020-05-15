a13493429097b2bb7bf5b2fe3cf79e2025e1c2d4
Ivan Tustanivskyi <itustanivsky@ninevastudios.com>
2020-05-15 11:17:07 +0300

    add method for retrieving user subscriptions

---
+++ Assets/Xsolla/Scripts/Api/Store/Entities/Subscription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Xsolla.Store
+{
+	[Serializable]
+	public class Subscription
+	{
+		public string sku;
+		public string name;
+		public string type;
+		public string description;
+		public string image_url;
+		public string status;
+		public long expired_at;
+		
+		[JsonProperty("class")]
+		public string subscription_class;
+	}
+}
