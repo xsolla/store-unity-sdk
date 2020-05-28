190f936521c11d5418d3910d9be91ae102f77252
Ivan Tustanivskyi <itustanivsky@ninevastudios.com>
2020-05-28 11:51:10 +0300

    add virtual item type field

--- Assets/Xsolla/Scripts/Api/Store/Entities/InventoryItem.cs
+++ Assets/Xsolla/Scripts/Api/Store/Entities/InventoryItem.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Xsolla.Store
 {
 	[Serializable]
 	public class InventoryItem
 	{
+		static readonly Dictionary<string, VirtualItemType> VirtualItemTypes =
+			new Dictionary<string, VirtualItemType>()
+			{
+				{"consumable", VirtualItemType.Consumable},
+				{"non_consumable", VirtualItemType.NonConsumable},
+				{"non_renewing_subscription", VirtualItemType.NonRenewingSubscription}
+			};
+
 		[Serializable]
 		public class Group
 		{
@@ -16,6 +26,7 @@ namespace Xsolla.Store
 		public string description;
 		//public object attributes; Don't use it yet.
 		public string type;
+		public string virtual_item_type;
 		public StoreItem.Group[] groups;
 		public string image_url;
 		public int? quantity;
@@ -24,7 +35,20 @@ namespace Xsolla.Store
 
 		public bool IsConsumable()
 		{
-			return remaining_uses != null;
+			return VirtualItemType == VirtualItemType.Consumable;
+		}
+		
+		public VirtualItemType VirtualItemType
+		{
+			get
+			{
+				if (virtual_item_type != null && VirtualItemTypes.Keys.Contains(virtual_item_type))
+				{
+					return VirtualItemTypes[virtual_item_type];
+				}
+
+				return VirtualItemType.None;
+			}
 		}
 	}
 }
