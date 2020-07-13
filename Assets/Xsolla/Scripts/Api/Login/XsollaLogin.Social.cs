b8cdc21f19ca6a08f128e2c61ead8eb48716cd7f
Levin Maxim <m.levin@xsolla.com>
2020-07-13 11:13:16 +0500

    change comments for Steam Auth

--- Assets/Xsolla/Scripts/Api/Login/XsollaLogin.Social.cs
+++ Assets/Xsolla/Scripts/Api/Login/XsollaLogin.Social.cs
@@ -19,10 +19,10 @@ namespace Xsolla.Login
 		/// Changes Steam session_ticket to JWT.
 		/// Note: this feature is not work "out the box" yet.
 		/// If you want to enable Steam auth, you need to contact with
-		/// support by email:<see cref="support@xsolla.com".
+		/// support by email:<see cref="support@xsolla.com"/>.
 		/// </summary>
-		/// <remarks> Swagger method name:<c>Cross-auth</c>.</remarks>
-		/// <see cref="https://developers.xsolla.com/login-api/jwt/cross-auth/"/>.
+		/// <remarks> Swagger method name:<c>Silent Authentication</c>.</remarks>
+		/// <see cref="https://developers.xsolla.com/login-api/methods/jwt/jwt-silent-authentication"/>.
 		/// <param name="appId">Your application Steam AppID.</param>
 		/// <param name="sessionTicket">Requested user's session_ticket by SteamAPI.</param>
 		/// <param name="onSuccess">Success operation callback.</param>
