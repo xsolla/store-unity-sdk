36bd61231be441039ce60850f87053e167aa97ee
Levin Maxim <m.levin@xsolla.com>
2020-08-13 15:04:18 +0500

    add invalid settings popup

--- Assets/Xsolla/Demo/StoreDemo/Scripts/DemoImplementation.cs
+++ Assets/Xsolla/Demo/StoreDemo/Scripts/DemoImplementation.cs
@@ -15,6 +15,30 @@ public partial class DemoImplementation : MonoBehaviour, IDemoImplementation
 				XsollaLogin.Instance.Token = XsollaStore.Instance.Token = GetUserToken();
 			}
 		};
+		ValidateXsollaSettings();
+	}
+
+	private void ValidateXsollaSettings()
+	{
+		var error = Error.UnknownError;
+		
+		if (XsollaSettings.LoginId == Constants.DEFAULT_LOGIN_ID || 
+		    XsollaSettings.LoginId == Constants.DEFAULT_PLATFORM_LOGIN_ID)
+		{
+			if (XsollaSettings.StoreProjectId != Constants.DEFAULT_PROJECT_ID)
+			{
+				error.errorMessage = $"You change [XsollaSettings->ProjectID] to '{XsollaSettings.StoreProjectId}', but not change LoginID. Change LoginID from '{XsollaSettings.LoginId}' to correct value.";
+				StoreDemoPopup.ShowError(error);
+			}
+		}
+		else
+		{
+			if (XsollaSettings.StoreProjectId == Constants.DEFAULT_PROJECT_ID)
+			{
+				error.errorMessage = $"You change [XsollaSettings->LoginID] to '{XsollaSettings.LoginId}', but not change ProjectID. Change ProjectID from '{XsollaSettings.StoreProjectId}' to correct value.";
+				StoreDemoPopup.ShowError(error);
+			}
+		}
 	}
 
 	public Token GetUserToken()
