3c37d5af18e415c3d2033a2910e6198857a1ed3d
Levin Maxim <m.levin@xsolla.com>
2020-08-31 11:21:14 +0500

    fix JWT parse from command line

--- Assets/Xsolla/Demo/LoginDemo/Scripts/Auth/Launcher/LauncherArguments.cs
+++ Assets/Xsolla/Demo/LoginDemo/Scripts/Auth/Launcher/LauncherArguments.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 using Xsolla.Core;
 
 public class LauncherArguments : MonoSingleton<LauncherArguments>
 {
-    const string LAUNCHER_TOKEN = "xsolla-login-token";
+    const string LAUNCHER_TOKEN = "xsolla-login-jwt";
 
     private bool invalidated = false;
 
@@ -15,17 +13,10 @@ public class LauncherArguments : MonoSingleton<LauncherArguments>
 		if(invalidated) {
             return "";
 		}
-        List<string> args = Environment.GetCommandLineArgs().ToList();
-        for (int i = 0; i < args.Count; i++) {
-			if(args[i].Contains(LAUNCHER_TOKEN)) {
-				if((i + 1) < args.Count) {
-                    return args[i + 1];
-                }
-			}
-		}
-
-        return "";
-    }
+		var args = Environment.GetCommandLineArgs().ToList();
+        var str = args.First(a => a.Contains(LAUNCHER_TOKEN));
+        return !string.IsNullOrEmpty(str) ? str.Split('=')[1] : string.Empty;
+	}
 
 	public void InvalidateTokenArguments()
 	{
