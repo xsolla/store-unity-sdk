f03cebd978076e5c19b0bf70a632961cc6eb6d93
Alexey Uporov <69147781+AlexeyUporov@users.noreply.github.com>
2020-11-16 14:03:32 +0500

    Update XsollaLogin.Social.cs

--- Assets/Xsolla/Scripts/Api/Login/XsollaLogin.Social.cs
+++ Assets/Xsolla/Scripts/Api/Login/XsollaLogin.Social.cs
@@ -27,16 +27,16 @@ namespace Xsolla.Login
 
 		/// <summary>
 		/// Changes Steam session_ticket to JWT.
-		/// Note: this feature is not work "out the box" yet.
-		/// If you want to enable Steam auth, you need to contact with
-		/// support by email:<see cref="support@xsolla.com"/>.
+		/// Note: this feature doesn't work "out the box" yet.
+		/// If you want to enable Steam auth, you need to contact the
+		/// support team by email:<see cref="support@xsolla.com"/>.
 		/// </summary>
 		/// <remarks> Swagger method name:<c>Silent Authentication</c>.</remarks>
 		/// <see cref="https://developers.xsolla.com/login-api/methods/jwt/jwt-silent-authentication"/>.
 		/// <see cref="https://developers.xsolla.com/login-api/methods/oauth-20/oauth-20-silent-authentication"/>.
 		/// <param name="appId">Your application Steam AppID.</param>
 		/// <param name="sessionTicket">Requested user's session_ticket by SteamAPI.</param>
-		/// <param name="onSuccess">Success operation callback.</param>
+		/// <param name="onSuccess">Successful operation callback.</param>
 		/// <param name="onError">Failed operation callback.</param>
 		public void SteamAuth(string appId, string sessionTicket, Action<string> onSuccess = null, Action<Error> onError = null)
 		{
@@ -80,7 +80,7 @@ namespace Xsolla.Login
 		}
 
 		/// <summary>
-		/// Get `url` for social auth in browser.
+		/// Get `url` for social authentication in browser.
 		/// </summary>
 		/// <remarks> Swagger method name:<c>Auth via Social Network</c>.</remarks>
 		/// <see cref="https://developers.xsolla.com/login-api/jwt/jwt-auth-via-social-network/"/>.
