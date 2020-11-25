625ee400bdb061298b826bb7b0cd21dbbd175c08
Ivan Tustanivskyi <itustanivsky@ninevastudios.com>
2020-11-25 15:04:01 +0200

    add missing cart methods

--- Assets/Xsolla/Scripts/Api/Store/XsollaStore.Cart.cs
+++ Assets/Xsolla/Scripts/Api/Store/XsollaStore.Cart.cs
@@ -9,17 +9,26 @@ namespace Xsolla.Store
 	public partial class XsollaStore : MonoSingleton<XsollaStore>
 	{
 		private const string URL_CART_CREATE_NEW = BASE_STORE_API_URL + "/cart";
-		private const string URL_CART_ITEM_UPDATE = BASE_STORE_API_URL + "/cart/{1}/item/{2}";
-		private const string URL_CART_ITEM_REMOVE = BASE_STORE_API_URL + "/cart/{1}/item/{2}";
+
+		private const string URL_CART_CURRENT_ITEM_UPDATE = BASE_STORE_API_URL + "/cart/item/{1}";
+		private const string URL_CART_SPECIFIC_ITEM_UPDATE = BASE_STORE_API_URL + "/cart/{1}/item/{2}";
+
+		private const string URL_CART_CURRENT_ITEM_REMOVE = BASE_STORE_API_URL + "/cart/item/{1}";
+		private const string URL_CART_SPECIFIC_ITEM_REMOVE = BASE_STORE_API_URL + "/cart/{1}/item/{2}";
+
 		private const string URL_CART_GET_ITEMS = BASE_STORE_API_URL + "/cart/{1}";
-		private const string URL_CART_CLEAR = BASE_STORE_API_URL + "/cart/{1}/clear";
-		private const string URL_CART_FILL = BASE_STORE_API_URL + "/cart/fill";
+
+		private const string URL_CART_CURRENT_CLEAR = BASE_STORE_API_URL + "/cart/clear";
+		private const string URL_CART_SPECIFIC_CLEAR = BASE_STORE_API_URL + "/cart/{1}/clear";
+
+		private const string URL_CART_CURRENT_FILL = BASE_STORE_API_URL + "/cart/fill";
+		private const string URL_CART_SPECIFIC_FILL = BASE_STORE_API_URL + "/cart/{1}/fill";
 
 		/// <summary>
 		/// Creates a new cart on the Xsolla side.
 		/// </summary>
 		/// <remarks> Swagger method name:<c>Get current user's cart</c>.</remarks>
-		/// <see cref="https://developers.xsolla.com/store-api/cart/get-user-cart"/>
+		/// <see cref="https://developers.xsolla.com/store-api/cart-payment/cart/get-user-cart/"/>
 		/// <param name="projectId">Project ID from your Publisher Account.</param>
 		/// <param name="onSuccess">Success operation callback.</param>
 		/// <param name="onError">Failed operation callback.</param>
@@ -30,27 +39,63 @@ namespace Xsolla.Store
 		}
 
 		/// <summary>
-		/// Fills the cart with items. If the cart already has an item,
+		/// Fills the current cart with items. If the cart already has an item,
 		/// the existing item will be replaced by the given value.
 		/// </summary>
-		/// <remarks> Swagger method name:<c>Fills the cart with items</c>.</remarks>
-		/// <see cref="https://developers.xsolla.com/store-api/cart/cart-fill"/>
+		/// <remarks> Swagger method name:<c>Fill cart with items</c>.</remarks>
+		/// <see cref="https://developers.xsolla.com/store-api/cart-payment/cart/cart-fill/"/>
 		/// <param name="projectId">Project ID from your Publisher Account.</param>
 		/// <param name="items">Items for purchase.</param>
 		/// <param name="onSuccess">Success operation callback.</param>
 		/// <param name="onError">Failed operation callback.</param>
 		public void FillCart(string projectId, List<CartFillItem> items, [NotNull] Action onSuccess, [CanBeNull] Action<Error> onError)
 		{
-			var urlBuilder = new StringBuilder(string.Format(URL_CART_FILL, projectId)).Append(AnalyticUrlAddition);
-			var entity = new CartFillEntity { items = items };
+			var urlBuilder = new StringBuilder(string.Format(URL_CART_CURRENT_FILL, projectId)).Append(AnalyticUrlAddition);
+			var entity = new CartFillEntity {items = items};
 			WebRequestHelper.Instance.PutRequest(urlBuilder.ToString(), entity, AuthAndAnalyticHeaders, onSuccess, onError, Error.CreateCartErrors);
 		}
 
 		/// <summary>
-		/// Updates an existing item or creates the one in the cart.
+		/// Fills the specific cart with items. If the cart already has an item,
+		/// the existing item will be replaced by the given value.
+		/// </summary>
+		/// <remarks> Swagger method name:<c>Fill specific cart with items</c>.</remarks>
+		/// <see cref="https://developers.xsolla.com/store-api/cart-payment/cart/cart-fill-by-id/"/>
+		/// <param name="projectId">Project ID from your Publisher Account.</param>
+		/// <param name="cartId">Unique cart identifier.</param>
+		/// <param name="items">Items for purchase.</param>
+		/// <param name="onSuccess">Success operation callback.</param>
+		/// <param name="onError">Failed operation callback.</param>
+		public void FillCart(string projectId, string cartId, List<CartFillItem> items, [NotNull] Action onSuccess, [CanBeNull] Action<Error> onError)
+		{
+			var urlBuilder = new StringBuilder(string.Format(URL_CART_SPECIFIC_FILL, projectId, cartId)).Append(AnalyticUrlAddition);
+			var entity = new CartFillEntity {items = items};
+			WebRequestHelper.Instance.PutRequest(urlBuilder.ToString(), entity, AuthAndAnalyticHeaders, onSuccess, onError, Error.CreateCartErrors);
+		}
+
+		/// <summary>
+		/// Updates an existing item or creates the one in the current cart.
+		/// </summary>
+		/// <remarks> Swagger method name:<c>Update cart line item from current cart</c>.</remarks>
+		/// <see cref="https://developers.xsolla.com/store-api/cart-payment/cart/put-item/"/>
+		/// <param name="projectId">Project ID from your Publisher Account.</param>
+		/// <param name="itemSku">SKU of item for purchase.</param>
+		/// <param name="quantity">Quantity of purchased item.</param>
+		/// <param name="onSuccess">Success operation callback.</param>
+		/// <param name="onError">Failed operation callback.</param>
+		/// <seealso cref="CreateNewCart"/>
+		public void UpdateItemInCart(string projectId, string itemSku, int quantity, [CanBeNull] Action onSuccess, [CanBeNull] Action<Error> onError)
+		{
+			var urlBuilder = new StringBuilder(string.Format(URL_CART_CURRENT_ITEM_UPDATE, projectId, itemSku)).Append(AnalyticUrlAddition);
+			var jsonObject = new Quantity {quantity = quantity};
+			WebRequestHelper.Instance.PutRequest<Quantity>(urlBuilder.ToString(), jsonObject, AuthAndAnalyticHeaders, onSuccess, onError, Error.AddToCartCartErrors);
+		}
+
+		/// <summary>
+		/// Updates an existing item or creates the one in the specific cart.
 		/// </summary>
 		/// <remarks> Swagger method name:<c>Update cart line item by cart ID</c>.</remarks>
-		/// <see cref="https://developers.xsolla.com/store-api/cart/put-item-by-cart-id"/>
+		/// <see cref="https://developers.xsolla.com/store-api/cart-payment/cart/put-item-by-cart-id/"/>
 		/// <param name="projectId">Project ID from your Publisher Account.</param>
 		/// <param name="cartId">Unique cart identifier.</param>
 		/// <param name="itemSku">SKU of item for purchase.</param>
@@ -60,23 +105,37 @@ namespace Xsolla.Store
 		/// <seealso cref="CreateNewCart"/>
 		public void UpdateItemInCart(string projectId, string cartId, string itemSku, int quantity, [CanBeNull] Action onSuccess, [CanBeNull] Action<Error> onError)
 		{
-			var urlBuilder = new StringBuilder(string.Format(URL_CART_ITEM_UPDATE, projectId, cartId, itemSku)).Append(AnalyticUrlAddition);
-			var jsonObject = new Quantity { quantity = quantity };
+			var urlBuilder = new StringBuilder(string.Format(URL_CART_SPECIFIC_ITEM_UPDATE, projectId, cartId, itemSku)).Append(AnalyticUrlAddition);
+			var jsonObject = new Quantity {quantity = quantity};
 			WebRequestHelper.Instance.PutRequest<Quantity>(urlBuilder.ToString(), jsonObject, AuthAndAnalyticHeaders, onSuccess, onError, Error.AddToCartCartErrors);
 		}
 
 		/// <summary>
-		/// Deletes all cart items.
+		/// Deletes all current cart items.
+		/// </summary>
+		/// <remarks> Swagger method name:<c>Delete all cart line items from current cart</c>.</remarks>
+		/// <see cref="https://developers.xsolla.com/store-api/cart-payment/cart/cart-clear/"/>
+		/// <param name="projectId">Project ID from your Publisher Account.</param>
+		/// <param name="onSuccess">Success operation callback.</param>
+		/// <param name="onError">Failed operation callback.</param>
+		public void ClearCart(string projectId, [CanBeNull] Action onSuccess, [CanBeNull] Action<Error> onError)
+		{
+			var urlBuilder = new StringBuilder(string.Format(URL_CART_CURRENT_CLEAR, projectId)).Append(AnalyticUrlAddition);
+			WebRequestHelper.Instance.PutRequest<Quantity>(urlBuilder.ToString(), null, AuthAndAnalyticHeaders, onSuccess, onError, Error.AddToCartCartErrors);
+		}
+
+		/// <summary>
+		/// Deletes all specific cart items.
 		/// </summary>
 		/// <remarks> Swagger method name:<c>Delete all cart line items by cart ID</c>.</remarks>
-		/// <see cref="https://developers.xsolla.com/store-api/cart/cart-clear-by-id"/>
+		/// <see cref="https://developers.xsolla.com/store-api/cart-payment/cart/cart-clear-by-id/"/>
 		/// <param name="projectId">Project ID from your Publisher Account.</param>
 		/// <param name="cartId">Unique cart identifier.</param>
 		/// <param name="onSuccess">Success operation callback.</param>
 		/// <param name="onError">Failed operation callback.</param>
 		public void ClearCart(string projectId, string cartId, [CanBeNull] Action onSuccess, [CanBeNull] Action<Error> onError)
 		{
-			var urlBuilder = new StringBuilder(string.Format(URL_CART_CLEAR, projectId, cartId)).Append(AnalyticUrlAddition);
+			var urlBuilder = new StringBuilder(string.Format(URL_CART_SPECIFIC_CLEAR, projectId, cartId)).Append(AnalyticUrlAddition);
 			WebRequestHelper.Instance.PutRequest<Quantity>(urlBuilder.ToString(), null, AuthAndAnalyticHeaders, onSuccess, onError, Error.AddToCartCartErrors);
 		}
 
@@ -84,7 +143,7 @@ namespace Xsolla.Store
 		/// Returns a user’s cart by ID.
 		/// </summary>
 		/// <remarks> Swagger method name:<c>Get cart by ID</c>.</remarks>
-		/// <see cref="https://developers.xsolla.com/store-api/cart/get-cart-by-id"/>
+		/// <see cref="https://developers.xsolla.com/store-api/cart-payment/cart/get-cart-by-id/"/>
 		/// <param name="projectId">Project ID from your Publisher Account.</param>
 		/// <param name="cartId">Unique cart identifier.</param>
 		/// <param name="onSuccess">Success operation callback.</param>
@@ -100,10 +159,25 @@ namespace Xsolla.Store
 		}
 
 		/// <summary>
-		/// Delete item from the cart.
+		/// Delete item from the current cart.
+		/// </summary>
+		/// <remarks> Swagger method name:<c>Delete cart line item from current cart</c>.</remarks>
+		/// <see cref="https://developers.xsolla.com/store-api/cart-payment/cart/delete-item/"/>
+		/// <param name="projectId">Project ID from your Publisher Account.</param>
+		/// <param name="itemSku">Item SKU to delete.</param>
+		/// <param name="onSuccess">Success operation callback.</param>
+		/// <param name="onError">Failed operation callback.</param>
+		public void RemoveItemFromCart(string projectId, string itemSku, [CanBeNull] Action onSuccess, [CanBeNull] Action<Error> onError)
+		{
+			var urlBuilder = new StringBuilder(string.Format(URL_CART_CURRENT_ITEM_REMOVE, projectId, itemSku)).Append(AnalyticUrlAddition);
+			WebRequestHelper.Instance.DeleteRequest(urlBuilder.ToString(), AuthAndAnalyticHeaders, onSuccess, onError, Error.DeleteFromCartErrors);
+		}
+
+		/// <summary>
+		/// Delete item from the specific cart.
 		/// </summary>
 		/// <remarks> Swagger method name:<c>Delete cart line item by cart ID</c>.</remarks>
-		/// <see cref="https://developers.xsolla.com/store-api/cart/delete-item-by-cart-id"/>
+		/// <see cref="https://developers.xsolla.com/store-api/cart-payment/cart/delete-item-by-cart-id/"/>
 		/// <param name="projectId">Project ID from your Publisher Account.</param>
 		/// <param name="cartId">Unique cart identifier.</param>
 		/// <param name="itemSku">Item SKU to delete.</param>
@@ -111,7 +185,7 @@ namespace Xsolla.Store
 		/// <param name="onError">Failed operation callback.</param>
 		public void RemoveItemFromCart(string projectId, string cartId, string itemSku, [CanBeNull] Action onSuccess, [CanBeNull] Action<Error> onError)
 		{
-			var urlBuilder = new StringBuilder(string.Format(URL_CART_ITEM_REMOVE, projectId, cartId, itemSku)).Append(AnalyticUrlAddition);
+			var urlBuilder = new StringBuilder(string.Format(URL_CART_SPECIFIC_ITEM_REMOVE, projectId, cartId, itemSku)).Append(AnalyticUrlAddition);
 			WebRequestHelper.Instance.DeleteRequest(urlBuilder.ToString(), AuthAndAnalyticHeaders, onSuccess, onError, Error.DeleteFromCartErrors);
 		}
 	}
