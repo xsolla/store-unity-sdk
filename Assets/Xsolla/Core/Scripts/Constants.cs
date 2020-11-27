63310c1923714a014185105deeb2fb8d17428a72
Alexey <a.demonov@gmail.com>
2020-11-27 18:25:51 +0500

    Update constants

--- Assets/Xsolla/Core/Scripts/Constants.cs
+++ Assets/Xsolla/Core/Scripts/Constants.cs
@@ -5,8 +5,8 @@
 		public const string LoginSdkVersion = "0.6.2.0";
 		public const string StoreSdkVersion = "0.6.2.0";
 
-		public const string DEFAULT_PROJECT_ID = "44056";
-		public const string DEFAULT_LOGIN_ID = "e6dfaac6-78a8-11e9-9244-42010aa80004";
+		public const string DEFAULT_PROJECT_ID = "77640";
+		public const string DEFAULT_LOGIN_ID = "026201e3-7e40-11ea-a85b-42010aa80004";
 		public const string DEFAULT_PLATFORM_LOGIN_ID = "5c5d2ca4-5fa6-11ea-b687-42010aa80004";
 
 		public const string LAST_SUCCESS_AUTH_TOKEN = "xsolla_login_last_success_auth_token";
