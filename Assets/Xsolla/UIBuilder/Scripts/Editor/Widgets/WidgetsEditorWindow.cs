b7b5dd09612a46039edeb6f11c27584f6c1e2644
Denis Rakhimov <d.rakhimov@xsolla.com>
2021-03-05 18:31:14 +0300

    Add UI Widgets data and components Re-organize UI Buidler assets

---
+++ Assets/Xsolla/UIBuilder/Scripts/Editor/Widgets/WidgetsEditorWindow.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Xsolla.UIBuilder
+{
+	public class WidgetsEditorWindow : EditorWindow
+	{
+		private readonly WidgetsListDrawer WidgetsDrawer = new WidgetsListDrawer();
+
+		private void OnGUI()
+		{
+			EditorGUILayout.Space();
+			WidgetsDrawer.Draw(this);
+
+			if (GUILayout.Button("Refresh"))
+			{
+				EditorUtility.SetDirty(WidgetsLibrary.Instance);
+				HandleSceneObjects();
+				HandleAssets();
+			}
+		}
+
+		private void HandleSceneObjects()
+		{
+			var containers = FindObjectsOfType<WidgetContainer>();
+			foreach (var container in containers)
+			{
+				if (!PrefabUtility.IsPartOfAnyPrefab(container))
+				{
+					WidgetsHandler.Handle(container);
+				}
+			}
+		}
+
+		private void HandleAssets()
+		{
+			var guids = AssetDatabase.FindAssets(
+				"t:prefab",
+				new[]
+				{
+					"Assets"
+				}
+			);
+
+			var containers = new List<WidgetContainer>();
+			foreach (var guid in guids)
+			{
+				var assetPath = AssetDatabase.GUIDToAssetPath(guid);
+				var container = AssetDatabase.LoadAssetAtPath<WidgetContainer>(assetPath);
+				if (container)
+				{
+					containers.Add(container);
+				}
+			}
+
+			foreach (var container in containers)
+			{
+				WidgetsHandler.Handle(container);
+			}
+		}
+
+		[MenuItem("Window/Xsolla/UI Widgets Editor")]
+		private static void OpenWindow()
+		{
+			GetWindow<WidgetsEditorWindow>("UI Widgets Editor");
+		}
+	}
+}
