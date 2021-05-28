ddbfb426a96c2fc4e4186570eb4bd183036b75d7
Alexey <a.demonov@gmail.com>
2021-05-28 15:42:16 +0500

    Revoke MenuStateMachine division

--- Assets/Xsolla/Login/Scripts/MenuStateMachine/MenuStateMachine.Auth.cs
+++ Assets/Xsolla/Login/Scripts/MenuStateMachine/MenuStateMachine.Auth.cs
@@ -2,20 +2,9 @@ using UnityEngine;
 
 namespace Xsolla.Demo
 {
-	[RequireComponent(typeof(MenuStateMachine))]
-	public class MenuStateMachineAuth : MonoBehaviour
+	public partial class MenuStateMachine : MonoBehaviour
 	{
-		private MenuStateMachine _stateMachine;
-
-		private bool IsAuthInProgress => _stateMachine.StateObject.GetComponent<LoginPageEnterController>()?.IsAuthInProgress ?? false;
-
-		private void Awake()
-		{
-			_stateMachine = GetComponent<MenuStateMachine>();
-			_stateMachine.CheckAuthorizationState += CheckAuthorizationState;
-			_stateMachine.RunLoginProxy += RunLoginProxy;
-			_stateMachine.ShowLoginError += ShowLoginError;
-		}
+		private bool IsAuthInProgress => _stateObject.GetComponent<LoginPageEnterController>()?.IsAuthInProgress ?? false;
 
 		private void CheckAuthorizationState(MenuState state, GameObject go)
 		{
@@ -36,10 +25,10 @@ namespace Xsolla.Demo
 							if (IsAuthInProgress)
 								return;
 							else
-								_stateMachine.SetState(MenuState.Registration);
+								SetState(MenuState.Registration);
 						};
 					else
-						buttonsProvider.LogInButton.onClick += () => _stateMachine.SetState(MenuState.Authorization);
+						buttonsProvider.LogInButton.onClick += () => SetState(MenuState.Authorization);
 				}
 				if (buttonsProvider.DemoUserButton != null)
 					buttonsProvider.DemoUserButton.onClick += () =>
@@ -68,10 +57,10 @@ namespace Xsolla.Demo
 				{
 					if (pageController != null)
 					{
-						pageController.OnSuccess = () => _stateMachine.SetState(MenuState.Main);
+						pageController.OnSuccess = () => SetState(MenuState.Main);
 							pageController.OnError = err =>
 							{
-								var obj = _stateMachine.SetState(MenuState.AuthorizationFailed);
+								var obj = SetState(MenuState.AuthorizationFailed);
 								if (obj != null)
 									obj.GetComponent<LoginPageErrorShower>()?.ShowError(err);
 							};
@@ -82,24 +71,24 @@ namespace Xsolla.Demo
 						if (IsAuthInProgress)
 							return;
 						else
-							_stateMachine.SetState(MenuState.ChangePassword);
+							SetState(MenuState.ChangePassword);
 					};
 					break;
 				}
 				case MenuState.AuthorizationFailed:
 				{
 					if (buttonsProvider != null)
-						buttonsProvider.OKButton.onClick += () => _stateMachine.SetState(MenuState.Authorization);
+						buttonsProvider.OKButton.onClick += () => SetState(MenuState.Authorization);
 					break;
 				}
 				case MenuState.Registration:
 				{
 					if (pageController != null)
 					{
-						pageController.OnSuccess = () => _stateMachine.SetState(MenuState.RegistrationSuccess);
+						pageController.OnSuccess = () => SetState(MenuState.RegistrationSuccess);
 						pageController.OnError = err =>
 						{
-							var obj = _stateMachine.SetState(MenuState.RegistrationFailed);
+							var obj = SetState(MenuState.RegistrationFailed);
 							if(obj != null)
 								obj.GetComponent<LoginPageErrorShower>()?.ShowError(err);
 						};
@@ -109,23 +98,23 @@ namespace Xsolla.Demo
 				case MenuState.RegistrationSuccess:
 				{
 					if (buttonsProvider != null)
-						buttonsProvider.OKButton.onClick += () => _stateMachine.SetState(MenuState.Authorization);
+						buttonsProvider.OKButton.onClick += () => SetState(MenuState.Authorization);
 					break;
 				}
 				case MenuState.RegistrationFailed:
 				{
 					if (buttonsProvider != null)
-						buttonsProvider.OKButton.onClick += () => _stateMachine.SetState(MenuState.Registration);
+						buttonsProvider.OKButton.onClick += () => SetState(MenuState.Registration);
 					break;
 				}
 				case MenuState.ChangePassword:
 				{
 					if (pageController != null)
 					{
-						pageController.OnSuccess = () => _stateMachine.SetState(MenuState.ChangePasswordSuccess);
+						pageController.OnSuccess = () => SetState(MenuState.ChangePasswordSuccess);
 						pageController.OnError = err =>
 						{
-							var obj = _stateMachine.SetState(MenuState.ChangePasswordFailed);
+							var obj = SetState(MenuState.ChangePasswordFailed);
 							if (obj != null)
 								obj.GetComponent<LoginPageErrorShower>()?.ShowError(err);
 						};
@@ -135,42 +124,18 @@ namespace Xsolla.Demo
 				case MenuState.ChangePasswordSuccess:
 				{
 					if (buttonsProvider)
-						buttonsProvider.OKButton.onClick += () => _stateMachine.SetState(MenuState.Authorization);
+						buttonsProvider.OKButton.onClick += () => SetState(MenuState.Authorization);
 					break;
 				}
 				case MenuState.ChangePasswordFailed:
 				{
 					if (buttonsProvider != null)
-						buttonsProvider.OKButton.onClick += () => _stateMachine.SetState(MenuState.ChangePassword);
+						buttonsProvider.OKButton.onClick += () => SetState(MenuState.ChangePassword);
 					break;
 				}
 			}
 
-			_stateMachine.ClearTrace();
-		}
-
-		private void RunLoginProxy()
-		{
-			var proxyScript = FindObjectOfType<LoginProxyActionHolder>();
-			var loginEnterScript = _stateMachine.StateObject.GetComponent<LoginPageEnterController>();
-
-			if (proxyScript != null && loginEnterScript != null)
-				loginEnterScript.RunLoginProxyAction(proxyScript.ProxyAction, proxyScript.ProxyActionArgument);
-
-			if (proxyScript != null)
-				Destroy(proxyScript.gameObject);
-		}
-
-		private void ShowLoginError()
-		{
-			var proxyScript = FindObjectOfType<LoginSettingsErrorHolder>();
-			var errorShower = _stateMachine.StateObject.GetComponent<LoginPageErrorShower>();
-
-			if (proxyScript != null && errorShower != null)
-				errorShower.ShowError(proxyScript.LoginSettingsError);
-
-			if (proxyScript != null)
-				Destroy(proxyScript.gameObject);
+			ClearTrace();
 		}
 	}
 }
