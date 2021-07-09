96b9c8e4e7a5ec68ecf12ee69f164972be35b44b
Ivan Tustanivskyi <tustanivsky@gmail.com>
2021-07-09 14:30:13 +0300

    Fix scroll issue on some pages

--- Assets/Xsolla/Core/Browser/XsollaBrowser/Examples/2D/SinglePageBrowser2D/UserDefinedBehaviours/MouseBehaviour2D.cs
+++ Assets/Xsolla/Core/Browser/XsollaBrowser/Examples/2D/SinglePageBrowser2D/UserDefinedBehaviours/MouseBehaviour2D.cs
@@ -21,7 +21,7 @@ namespace Xsolla.Core.Browser
 		Coroutine mouseMovementCoroutine;
 		Canvas canvas;
 		Camera canvasCamera;
-		
+
 		public int ScrollSpeed { get; set; } = -75;
 
 		private void Awake()
@@ -33,24 +33,27 @@ namespace Xsolla.Core.Browser
 			browserMouse = gameObject.GetComponent<XsollaBrowser>().Input.Mouse;
 			display = gameObject.GetComponent<Display2DBehaviour>();
 		}
-	
+
 
 		private void OnDestroy()
 		{
 			StopAllCoroutines();
 		}
-	
+
 		void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
 		{
-			if (mouseMovementCoroutine != null) {
+			if (mouseMovementCoroutine != null)
+			{
 				StopCoroutine(mouseMovementCoroutine);
 			}
+
 			mouseMovementCoroutine = StartCoroutine(MouseCoroutine());
 		}
 
 		void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
 		{
-			if (mouseMovementCoroutine != null) {
+			if (mouseMovementCoroutine != null)
+			{
 				StopCoroutine(mouseMovementCoroutine);
 				mouseMovementCoroutine = null;
 			}
@@ -61,17 +64,22 @@ namespace Xsolla.Core.Browser
 			Vector2 mousePosition = GetMousePosition(eventData.position);
 			browserMouse.Click(mousePosition, (Vector2 pos) => Debug.Log("Click handled by: " + pos.ToString()));
 		}
-		
+
 		void IScrollHandler.OnScroll(PointerEventData eventData)
 		{
 			var scrollDelta = eventData.scrollDelta * ScrollSpeed;
-			browserMouse.Scroll(scrollDelta);
+
+			gameObject.GetComponent<XsollaBrowser>().Input.Keyboard.KeyDown(
+				scrollDelta.y > 0
+					? KeysConverter.Convert(KeyCode.DownArrow)
+					: KeysConverter.Convert(KeyCode.UpArrow));
 		}
 
 		IEnumerator MouseCoroutine()
 		{
 			Vector2 lastPosition = Vector2.zero;
-			while (true) {
+			while (true)
+			{
 				yield return Display2DInitializationCoroutine();
 				Vector2 mousePosition = GetMousePosition(InputProxy.MousePosition);
 				yield return MouseMovementCoroutine(lastPosition, mousePosition, (Vector2 pos) => lastPosition = pos);
@@ -80,43 +88,52 @@ namespace Xsolla.Core.Browser
 
 		IEnumerator Display2DInitializationCoroutine()
 		{
-			yield return new WaitWhile(() => (display.Width == 0) || (display.Height == 0)); 
+			yield return new WaitWhile(() => (display.Width == 0) || (display.Height == 0));
 		}
 
 		IEnumerator MouseMovementCoroutine(Vector2 lastPosition, Vector2 mousePosition, Action<Vector2> callback = null)
 		{
-			if ((mousePosition - lastPosition).magnitude > 1.0F) {
+			if ((mousePosition - lastPosition).magnitude > 1.0F)
+			{
 				yield return ActionExtensions.WaitMethod(
 					browserMouse.MoveTo, mousePosition, (Vector2 pos) => callback?.Invoke(pos)
 				);
-			} else {
+			}
+			else
+			{
 				yield return new WaitForFixedUpdate();
 			}
 		}
 
 		Vector2 GetMousePosition(Vector3 inputMousePosition)
 		{
-			RectTransform rect = (RectTransform)(canvas.transform);
-			if (RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, inputMousePosition, canvasCamera, out Vector2 canvasPoint)) {
-				rect = (RectTransform)transform;
-				Vector2 offset = canvasPoint - ((Vector2)rect.localPosition);
+			RectTransform rect = (RectTransform) (canvas.transform);
+			if (RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, inputMousePosition, canvasCamera, out Vector2 canvasPoint))
+			{
+				rect = (RectTransform) transform;
+				Vector2 offset = canvasPoint - ((Vector2) rect.localPosition);
 				Vector2 leftUpperCorner = rect.sizeDelta / 2;
 				Vector2 point = leftUpperCorner + offset;
 				return ConvertCoordinatesToPixels(point);
-			} else {
+			}
+			else
+			{
 				Debug.LogWarning("You try get mouse position, but mouse not over canvas");
 			}
+
 			return Vector2.zero;
 		}
 
 		Vector2 ConvertCoordinatesToPixels(Vector3 mousePositionOverScreen)
 		{
-			if (!mousePositionOverScreen.Equals(Vector3.zero)) {
+			if (!mousePositionOverScreen.Equals(Vector3.zero))
+			{
 				float x = (mousePositionOverScreen.x / browserCollider.size.x) * display.Width;
 				float y = (mousePositionOverScreen.y / browserCollider.size.y) * display.Height;
 				y = display.Height - y; // Because browser's axis are differs from Unity's axis
 				return new Vector2(x, y);
 			}
+
 			return Vector2.zero;
 		}
 	}
