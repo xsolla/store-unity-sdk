0cfc7e45d40f87a7ae7f74e0f13e892a731356cc
Alexey <a.demonov@gmail.com>
2021-10-21 12:07:18 +0500

    Remove copy to temp folder and autotests overlogging

--- cicd/autotests.bat
+++ cicd/autotests.bat
@@ -1,29 +1,18 @@
 @echo OFF
 
-echo "####        Autotests        ####"
 set PROJECT_PATH=%1
 set UNITY_VERSION=%2
 set UNITY_PATH="C:\Program Files\Unity\Hub\Editor\%UNITY_VERSION%\Editor\Unity.exe"
-set WORK_DIR=C:\Users\Runner\Desktop\RunnerWorkDir\ProjectTemp
 set LOGS_PATH=%PROJECT_PATH%\Logs\autotest_results.txt
-
-echo "####   Copy To Project Temp  ####"
-xcopy Assets\ %WORK_DIR%\Assets\ /e >nul 2>&1
-xcopy Packages\ %WORK_DIR%\Packages\ /e >nul 2>&1
-xcopy ProjectSettings\ %WORK_DIR%\ProjectSettings\ /e >nul 2>&1
 
 echo "####     Start Autotests     ####"
 echo "PROJECT_PATH: %PROJECT_PATH%"
 echo "UNITY_VERSION: %UNITY_VERSION%"
-echo "%UNITY_PATH% -batchmode -runTests -projectPath %WORK_DIR% -testResults %LOGS_PATH%"
-call %UNITY_PATH% -batchmode -runTests -projectPath %WORK_DIR% -testResults %LOGS_PATH%
-set TEST_ERROR=%errorlevel%
-echo "####      End Autotests      ####"
-echo "####      Error Level:%TEST_ERROR%      ####"
-echo "####        Full Log:        ####"
-for /F "tokens=* delims=" %%x in (%LOGS_PATH%) do echo %%x
+echo "%UNITY_PATH% -batchmode -runTests -projectPath %PROJECT_PATH% -testResults %LOGS_PATH%"
+call %UNITY_PATH% -batchmode -runTests -projectPath %PROJECT_PATH% -testResults %LOGS_PATH%
 
-echo "####     Remove WORK_DIR     ####"
-rmdir /s /q %WORK_DIR%
-
-exit /b %TEST_ERROR%
+if errorlevel 0 echo "####  All Tests Successful   ####"
+if errorlevel 2 echo "####  Some tests have failed, check the Logs  ####"
+if errorlevel 3 echo "####  RUN FAILURE, check the Logs  ####"
+echo "#### Autotests Ended With Errorlevel:%errorlevel% ####"
+exit /b %errorlevel%
