e7ee6835cd7551ea158fb0f83f751b48eb67ba95
Alexey <a.demonov@gmail.com>
2021-10-22 11:44:01 +0500

    Remove unused variable

--- cicd/build-universal.bat
+++ cicd/build-universal.bat
@@ -21,8 +21,6 @@ set BUILD_LOG_PATH=%BUILD_ROOT%\build.log
 set UNITY_PATH="C:\Program Files\Unity\Hub\Editor\%UNITY_VERSION%\Editor\Unity.exe"
 echo %UNITY_PATH%
 
-set LOGS_PATH=%PROJECT_PATH%\Logs
-
 echo "%UNITY_PATH% -batchmode -quit -projectPath %PROJECT_PATH% -customBuildPath "%BUILD_ROOT%" -customBuildTarget "%BUILD_TARGET%" -executeMethod Xsolla.BuildsManager.Build -logFile "%BUILD_LOG_PATH%""
 call %UNITY_PATH% -batchmode -quit -projectPath %PROJECT_PATH% -customBuildPath "%BUILD_ROOT%" -customBuildTarget "%BUILD_TARGET%" -executeMethod Xsolla.BuildsManager.Build -logFile "%BUILD_LOG_PATH%"
 
