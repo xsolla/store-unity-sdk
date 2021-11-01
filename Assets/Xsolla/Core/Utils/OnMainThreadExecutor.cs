6610d126c4c11bc4fd79514d1b014f77b02f4c82
Denis Rakhimov <d.rakhimov@xsolla.com>
2021-11-01 17:05:01 +0300

    Add websockets as order status checking method

---
+++ Assets/Xsolla/Core/Utils/OnMainThreadExecutor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Xsolla.Core
+{
+	public class OnMainThreadExecutor : MonoSingleton<OnMainThreadExecutor>
+	{
+		private readonly ConcurrentQueue<Action> RunOnMainThread = new ConcurrentQueue<Action>();
+
+		private void Update()
+		{
+			if (RunOnMainThread.IsEmpty)
+				return;
+
+			while (RunOnMainThread.TryDequeue(out var action))
+			{
+				action?.Invoke();
+			}
+		}
+
+		public void Enqueue(Action action)
+		{
+			Instance.RunOnMainThread.Enqueue(action);
+		}
+	}
+}
