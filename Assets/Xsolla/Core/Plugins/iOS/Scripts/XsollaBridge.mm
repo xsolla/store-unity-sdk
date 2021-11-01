05b8fd5212fc1adf2d0f337c32b008079859cee9
Ivan Tustanivskyi <tustanivsky@gmail.com>
2021-11-01 15:04:05 +0100

    Add mechanism allowing to receive callbacks from native iOS

--- Assets/Xsolla/Core/Plugins/iOS/Scripts/XsollaBridge.mm
+++ Assets/Xsolla/Core/Plugins/iOS/Scripts/XsollaBridge.mm
@@ -1,34 +1,45 @@
 #import <AuthenticationServices/AuthenticationServices.h>
+
 #import "XsollaSDKLoginKitUnity/XsollaSDKLoginKitUnity-Swift.h"
+#import "XsollaUtils.h"
 
 #pragma mark - C interface
 
+typedef void(ActionVoidCallbackDelegate)(void *actionPtr);
+typedef void(ActionStringCallbackDelegate)(void *actionPtr, const char *data);
+
 extern "C" {
 
-    void _xsolla_TestSwift() {
-		OAuth2Params *oauthParams = [[OAuth2Params alloc] initWithClientId:57
-															 state:@"xsollatest"
+    void _authBySocialNetwork(char* platform, int clientId, char* state, char* redirectUri,
+        ActionStringCallbackDelegate authSuccessCallback, void *authSuccessActionPtr, ActionVoidCallbackDelegate errorCallback, void *errorActionPtr) {
+		
+        NSString* platformString = [XsollaUtils createNSStringFrom:platform];
+        NSString* stateString = [XsollaUtils createNSStringFrom:state];
+        NSString* redirectUriString = [XsollaUtils createNSStringFrom:redirectUri];
+    
+		OAuth2Params *oauthParams = [[OAuth2Params alloc] initWithClientId:clientId
+															 state:stateString
 															 scope:@"offline"
-															 redirectUri:@"app://xsollalogin"];
+															 redirectUri:redirectUriString];
 		
 		JWTGenerationParams *jwtGenerationParams = [[JWTGenerationParams alloc] initWithGrantType:TokenGrantTypeAuthorizationCode
-															 clientId:57
+															 clientId:clientId
 															 refreshToken:nil
 															 clientSecret:nil
-															 redirectUri:@"app://xsollalogin"];
+															 redirectUri:redirectUriString];
 		
 		if (@available(iOS 13.4, *)) {
 			WebAuthenticationPresentationContextProvider* context = [[WebAuthenticationPresentationContextProvider alloc] initWithPresentationAnchor:UnityGetMainWindow()];
 			
-			[[LoginKitUnity shared] authBySocialNetwork:@"google" oAuth2Params:oauthParams jwtParams:jwtGenerationParams presentationContextProvider:context completion:^(AccessTokenInfo * _Nullable accesTokenInfo, NSError * _Nullable error){
+			[[LoginKitUnity shared] authBySocialNetwork:platformString oAuth2Params:oauthParams jwtParams:jwtGenerationParams presentationContextProvider:context completion:^(AccessTokenInfo * _Nullable accesTokenInfo, NSError * _Nullable error){
 
 				if(error != nil) {
-					NSLog(@"Error: %@", error);
+					errorCallback(errorActionPtr);
+					return;
 				}
-				else {
-					NSLog(@"Received token: %@", accesTokenInfo.accessToken);
-				}
-				
+
+				NSString* tokenInfoString = [XsollaUtils serializeTokenInfo:accesTokenInfo];
+				authSuccessCallback(authSuccessActionPtr, [XsollaUtils createCStringFrom:tokenInfoString]);
 			}];
 		} else {
 			NSLog(@"Authentication via social networks with Xsolla is not supported for current iOS version.");
