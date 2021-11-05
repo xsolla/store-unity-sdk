bf16b815120c399b23d939e91fdbff0559a4dc5c
Ivan Tustanivskyi <tustanivsky@gmail.com>
2021-11-05 11:58:22 +0100

    Remove redundant comment

--- Assets/Xsolla/Core/Plugins/iOS/Scripts/XsollaSDKLoginKitUnity-Bridging-Header.h
+++ Assets/Xsolla/Core/Plugins/iOS/Scripts/XsollaSDKLoginKitUnity-Bridging-Header.h
@@ -1,11 +1,3 @@
-//
-//  XsollaSDKLoginKitUnity-Bridging-Header.h
-//  XsollaSDKLoginKitUnity
-//
-//  Created by Ivan Tustanivsky on 08/10/21.
-//  Copyright © 2020 InBrain. All rights reserved.
-//
-
 #ifndef XsollaSDKLoginKitUnity_Bridging_Header_h
 #define XsollaSDKLoginKitUnity_Bridging_Header_h
 
