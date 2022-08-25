bc7a66e562abefdb9d0e8fa8c1e8977fae1f3a7c
Denis Rakhimov <d.rakhimov@xsolla.com>
2022-08-25 09:07:03 +0300

    Fix ios build process

--- Assets/Xsolla/Core/Editor/iOSBuildPostProcess.cs
+++ Assets/Xsolla/Core/Editor/iOSBuildPostProcess.cs
@@ -33,21 +33,6 @@ namespace Xsolla.Core.Editor
 #endif
 			
 			project.SetBuildProperty(targetGuid, "ENABLE_BITCODE", "NO");
-			
-			project.SetBuildProperty(targetGuid, "SWIFT_OBJC_BRIDGING_HEADER", "Libraries/Xsolla/Core/Plugins/iOS/Scripts/XsollaSDKLoginKitObjectiveC-Bridging-Header.h");
-			project.SetBuildProperty(targetGuid, "SWIFT_OBJC_INTERFACE_HEADER_NAME", "XsollaSDKLoginKitObjectiveC-Swift.h");
-			
-			project.SetBuildProperty(targetGuid, "SWIFT_OBJC_BRIDGING_HEADER", "Libraries/Xsolla/Core/Plugins/iOS/Scripts/XsollaSDKPaymentsKitObjectiveC-Bridging-Header.h");
-			project.SetBuildProperty(targetGuid, "SWIFT_OBJC_INTERFACE_HEADER_NAME", "XsollaSDKPaymentsKitObjectiveC-Swift.h");
-
-			project.AddBuildProperty(targetGuid, "LD_RUNPATH_SEARCH_PATHS", "@executable_path/Frameworks $(PROJECT_DIR)/lib/$(CONFIGURATION) $(inherited)");
-			project.AddBuildProperty(targetGuid, "FRAMEWORK_SEARCH_PATHS", "$(inherited) $(PROJECT_DIR) $(PROJECT_DIR)/Frameworks");
-			project.AddBuildProperty(targetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
-			project.AddBuildProperty(targetGuid, "DYLIB_INSTALL_NAME_BASE", "@rpath");
-			project.AddBuildProperty(targetGuid, "LD_DYLIB_INSTALL_NAME", "@executable_path/../Frameworks/$(EXECUTABLE_PATH)");
-			project.AddBuildProperty(targetGuid, "DEFINES_MODULE", "YES");
-			project.AddBuildProperty(targetGuid, "SWIFT_VERSION", "5.0");
-			project.AddBuildProperty(targetGuid, "COREML_CODEGEN_LANGUAGE", "Swift");
 
 			try
 			{
