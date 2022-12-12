d112a6788bf64b4beaa19f5b6530178405e34d3b
Alexey <a.demonov@gmail.com>
2022-12-12 14:06:01 +0800

    Rework OrderTrackerByWebSockets

--- Assets/Xsolla/Orders/OrderTracking/OrderTrackerByWebsockets.cs
+++ Assets/Xsolla/Orders/OrderTracking/OrderTrackerByWebsockets.cs
@@ -1,19 +1,19 @@
-using System.Collections;
+using System;
 using Newtonsoft.Json;
-using UnityEngine;
 using WebSocketSharp;
 using Xsolla.Core;
+using UnityEngine;
 
 namespace Xsolla.Orders
 {
 	public class OrderTrackerByWebsockets : OrderTracker
 	{
 		private const string URL_PATTERN = "wss://store-ws.xsolla.com/sub/order/status?order_id={0}&project_id={1}";
-		private const int TIME_OUT_DURATION = 300;
+		private const float EXPECTED_TTL = 300f;
 
 		private WebSocket _webSocket;
-		private Coroutine _timeoutCoroutine;
 		private OnMainThreadExecutor _onMainThreadExecutor;
+		private float _startTime;
 
 		public override void Start()
 		{
@@ -21,16 +21,16 @@ namespace Xsolla.Orders
 			if (!_onMainThreadExecutor)
 				_onMainThreadExecutor = OrderTracking.Instance.gameObject.AddComponent<OnMainThreadExecutor>();
 
-			var url = string.Format(URL_PATTERN, TrackingData.orderId, TrackingData.projectId);
+			var url = string.Format(URL_PATTERN, base.TrackingData.orderId, base.TrackingData.projectId);
 			_webSocket = new WebSocket(url);
 			_webSocket.SslConfiguration.EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12;
 
 			_webSocket.OnMessage += OnWebSocketMessage;
-			_webSocket.OnClose += OnWebSocketClose;
-			_webSocket.OnError += OnWebSocketError;
+			_webSocket.OnClose += OnWebSocketInterrupt;
+			_webSocket.OnError += OnWebSocketInterrupt;
 			_webSocket.Connect();
 
-			_timeoutCoroutine = base.StartCoroutine(WaitTimeout());
+			_startTime = Time.time;
 		}
 
 		public override void Stop()
@@ -38,38 +38,38 @@ namespace Xsolla.Orders
 			if (_webSocket != null)
 			{
 				_webSocket.OnMessage -= OnWebSocketMessage;
-				_webSocket.OnClose -= OnWebSocketClose;
-				_webSocket.OnError -= OnWebSocketError;
+				_webSocket.OnClose -= OnWebSocketInterrupt;
+				_webSocket.OnError -= OnWebSocketInterrupt;
+				_webSocket.Close();
 				_webSocket = null;
 			}
-
-			if (_timeoutCoroutine != null)
-				base.StopCoroutine(_timeoutCoroutine);
 		}
 
-		private IEnumerator WaitTimeout()
-		{
-			yield return new WaitForSeconds(TIME_OUT_DURATION);
-			ReplaceSelfByShortPollingTracker();
-		}
-
-		private void ReplaceSelfByShortPollingTracker()
-		{
-			var shortPollingTracker = new OrderTrackerByShortPolling(TrackingData);
-			OrderTracking.Instance.ReplaceTracker(this, shortPollingTracker);
-		}
-
+		//TEXTREVIEW
 		private void OnWebSocketMessage(object sender, MessageEventArgs args)
 		{
 			_onMainThreadExecutor.Enqueue(() =>
 			{
-				var status = JsonConvert.DeserializeObject<OrderStatus>(args.Data);
+				OrderStatus status = null;
 
-				HandleOrderStatus(
+				try {
+					status = JsonConvert.DeserializeObject<OrderStatus>(args.Data);
+				} catch (System.Exception ex) {
+					Debug.LogError($"Could not parse WebSocket message with exception: {ex.Message}");
+				}
+
+				if (status == null) {
+					Debug.LogError($"WebSocket message order status is null");
+					return;
+				}
+
+				Debug.Log($"WebSocket status message. OrderID:'{status.order_id}' Status:'{status.status}'");
+
+				base.HandleOrderStatus(
 					status.status,
 					onDone: () =>
 					{
-						TrackingData.successCallback?.Invoke();
+						base.TrackingData.successCallback?.Invoke();
 						RemoveSelfFromTracking();
 					},
 					onCancel: RemoveSelfFromTracking
@@ -77,14 +77,43 @@ namespace Xsolla.Orders
 			});
 		}
 
-		private void OnWebSocketClose(object sender, CloseEventArgs args)
+		//TEXTREVIEW
+		private void OnWebSocketInterrupt(object sender, EventArgs args)
 		{
-			_onMainThreadExecutor.Enqueue(ReplaceSelfByShortPollingTracker);
-		}
+			_onMainThreadExecutor.Enqueue(() =>
+			{
+				if (args is CloseEventArgs closeArgs)
+					Debug.LogWarning($"WebSocket was closed with code:'{closeArgs.Code}', reason:'{closeArgs.Reason}', wasClean:'{closeArgs.WasClean}'");
+				else if (args is ErrorEventArgs errorArgs)
+					Debug.LogError($"WebSocket was closed with exception:'{errorArgs.Exception.Message}', message:'{errorArgs.Message}'");
+				else
+					Debug.LogWarning($"WebSocket was closed with unexpected argument '{args}'");
 
-		private void OnWebSocketError(object sender, ErrorEventArgs args)
-		{
-			_onMainThreadExecutor.Enqueue(ReplaceSelfByShortPollingTracker);
+				var webSocketTTL = Time.time - _startTime;
+
+				if (webSocketTTL >= EXPECTED_TTL)
+				{
+					Debug.Log($"WebSocket TTL '{webSocketTTL}' is OK, replacing interrupted websocket with a new one");
+
+					var newTracker = new OrderTrackerByWebsockets(base.TrackingData);
+
+					if (OrderTracking.Instance.ReplaceTracker(this, newTracker))
+						Debug.Log($"Traker for order {base.TrackingData.orderId} is replaced with a new one");
+					else
+						Debug.LogError($"Failed to relace tracker for order {base.TrackingData.orderId}");
+				}
+				else
+				{
+					Debug.LogWarning($"WebSocket TTL '{webSocketTTL}' is lower than expected, switching to short-polling");
+
+					var shortPollingTracker = new OrderTrackerByShortPolling(base.TrackingData);
+
+					if (OrderTracking.Instance.ReplaceTracker(this, shortPollingTracker))
+						Debug.Log($"Traker for order {base.TrackingData.orderId} is replaced with short polling");
+					else
+						Debug.LogError($"Failed to relace tracker for order {base.TrackingData.orderId}");
+				}
+			});
 		}
 
 		public OrderTrackerByWebsockets(OrderTrackingData trackingData) : base(trackingData)
