087e322e6b75fb419a0fb5adf143874ee5e93d0b
Mariia <m.makarova@xsolla.com>
2023-02-22 14:30:31 +0500

    Exp6 (Inventory methods)

--- Assets/Xsolla/Inventory/Api/XsollaInventory.cs
+++ Assets/Xsolla/Inventory/Api/XsollaInventory.cs
@@ -13,14 +13,14 @@ namespace Xsolla.Inventory
 		private const string URL_GET_TIME_LIMITED_ITEMS = Constants.BASE_STORE_API_URL + "/user/time_limited_items";
 
 		/// <summary>
-		/// Returns balance for all virtual currencies.
+		/// Returns the current user’s balance of virtual currency. For each virtual currency, complete data is returned.
 		/// </summary>
-		/// <remarks> Swagger method name:<c>Get the current user's virtual balance</c>.</remarks>
-		/// <see cref="https://developers.xsolla.com/store-api/inventory-client/get-user-virtual-balance"/>
+		/// <remarks>[More about the use cases](https://developers.xsolla.com/sdk/unity/player-inventory/display-inventory/).</remarks>
 		/// <param name="projectId">Project ID from your Publisher Account.</param>
 		/// <param name="onSuccess">Successful operation callback.</param>
-		/// <param name="onError">Called after the request resulted with an error.</param>
-		/// <param name="platform">Publishing platform the user plays on.</param>
+		/// <param name="onError">Called after virtual currency balance was successfully received.</param>
+		/// <param name="platform">Publishing platform the user plays on.<br/>
+		/// Can be `xsolla` (default), `playstation_network`, `xbox_live`, `pc_standalone`, `nintendo_shop`, `google_play`, `app_store_ios`, `android_standalone`, `ios_standalone`, `android_other`, `ios_other`, or `pc_other`.</param>
 		public void GetVirtualCurrencyBalance(string projectId, [NotNull] Action<VirtualCurrencyBalances> onSuccess, [CanBeNull] Action<Error> onError, string platform = null)
 		{
 			var url = string.Format(URL_VIRTUAL_CURRENCY_BALANCE, projectId);
@@ -32,17 +32,18 @@ namespace Xsolla.Inventory
 		}
 
 		/// <summary>
-		/// Retrieves the user’s inventory.
+		/// Returns a list of virtual items from the user’s inventory according to pagination settings. For each virtual item, complete data is returned.
 		/// </summary>
-		/// <remarks> Swagger method name:<c>Get the current user's inventory</c>.</remarks>
-		/// <see cref="https://developers.xsolla.com/store-api/inventory-client/get-user-inventory"/>
+		/// <remarks>[More about the use cases](https://developers.xsolla.com/sdk/unity/player-inventory/display-inventory/).</remarks>
 		/// <param name="projectId">Project ID from your Publisher Account.</param>
-		/// <param name="onSuccess">Successful operation callback.</param>
+		/// <param name="onSuccess">Called after purchased virtual items were successfully received.</param>
 		/// <param name="onError">Called after the request resulted with an error.</param>
 		/// <param name="limit">Limit for the number of elements on the page.</param>
 		/// <param name="offset">Number of the element from which the list is generated (the count starts from 0).</param>
-		/// <param name="locale">Defines localization of item's text fields.</param>
-		/// <param name="platform">Publishing platform the user plays on.</param>
+		/// <param name="locale">Defines localization of item's text fields.<br/>
+		/// The following languages are supported: Arabic (`ar`), Bulgarian (`bg`), Czech (`cs`), German (`de`), Spanish (`es`), French (`fr`), Hebrew (`he`), Italian (`it`), Japanese (`ja`), Korean (`ko`), Polish (`pl`), Portuguese (`pt`), Romanian (`ro`), Russian (`ru`), Thai (`th`), Turkish (`tr`), Vietnamese (`vi`), Chinese Simplified (`cn`), Chinese Traditional (`tw`), English (`en`, default).</param>
+		/// <param name="platform">Publishing platform the user plays on.<br/>
+		/// Can be `xsolla` (default), `playstation_network`, `xbox_live`, `pc_standalone`, `nintendo_shop`, `google_play`, `app_store_ios`, `android_standalone`, `ios_standalone`, `android_other`, `ios_other`, or `pc_other`.</param>
 		public void GetInventoryItems(string projectId, [NotNull] Action<InventoryItems> onSuccess, [CanBeNull] Action<Error> onError, int limit = 50, int offset = 0, [CanBeNull] string locale = null, string platform = null)
 		{
 			var url = string.Format(URL_INVENTORY_GET_ITEMS, projectId);
@@ -54,15 +55,15 @@ namespace Xsolla.Inventory
 		}
 
 		/// <summary>
-		/// Consumes item from inventory.
+		/// Consumes an inventory item. Use for only for consumable virtual items.
 		/// </summary>
-		/// <remarks> Swagger method name:<c>Consume item</c>.</remarks>
-		/// <see cref="https://developers.xsolla.com/store-api/inventory-client/consume-item"/>
+		/// <remarks>[More about the use cases](https://developers.xsolla.com/sdk/unity/player-inventory/consume-item/).</remarks>
 		/// <param name="projectId">Project ID from your Publisher Account.</param>
 		/// <param name="item">Contains consume parameters.</param>
-		/// <param name="onSuccess">Successful operation callback.</param>
+		/// <param name="onSuccess">Called after successful inventory item consumption.</param>
 		/// <param name="onError">Called after the request resulted with an error.</param>
-		/// <param name="platform">Publishing platform the user plays on.</param>
+		/// <param name="platform">Publishing platform the user plays on.<br/>
+		/// Can be `xsolla` (default), `playstation_network`, `xbox_live`, `pc_standalone`, `nintendo_shop`, `google_play`, `app_store_ios`, `android_standalone`, `ios_standalone`, `android_other`, `ios_other`, or `pc_other`.</param>
 		public void ConsumeInventoryItem(string projectId, ConsumeItem item, [CanBeNull] Action onSuccess, [CanBeNull] Action<Error> onError, string platform = null)
 		{
 			var url = string.Format(URL_INVENTORY_ITEM_CONSUME, projectId);
@@ -76,14 +77,14 @@ namespace Xsolla.Inventory
 		}
 
 		/// <summary>
-		/// Retrieves the current user’s time limited items.
+		/// Returns a list of time-limited items from the user’s inventory. For each item, complete data is returned.
 		/// </summary>
-		/// <remarks> Swagger method name:<c>Retrieves the current user’s time limited items.</c>.</remarks>
-		/// <see cref="https://developers.xsolla.com/store-api/inventory-client/get-user-subscriptions"/>
+		/// <remarks>[More about the use cases](https://developers.xsolla.com/sdk/unity/player-inventory/display-inventory/).</remarks>
 		/// <param name="projectId">Project ID from your Publisher Account.</param>
-		/// <param name="onSuccess">Success operation callback.</param>
+		/// <param name="onSuccess">Called after list of user time limited items was successfully received.</param>
 		/// <param name="onError">Called after the request resulted with an error.</param>
-		/// <param name="platform">Publishing platform the user plays on.</param>
+		/// <param name="platform">Publishing platform the user plays on.<br/>
+		/// Can be `xsolla` (default), `playstation_network`, `xbox_live`, `pc_standalone`, `nintendo_shop`, `google_play`, `app_store_ios`, `android_standalone`, `ios_standalone`, `android_other`, `ios_other`, or `pc_other`.</param>
 		public void GetTimeLimitedItems(string projectId, [NotNull] Action<TimeLimitedItems> onSuccess, [CanBeNull] Action<Error> onError, string platform = null)
 		{
 			var url = string.Format(URL_GET_TIME_LIMITED_ITEMS, projectId);
