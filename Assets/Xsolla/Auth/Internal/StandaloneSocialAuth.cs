976174e424bb812d441136a50953ccf873e54d66
Denis Rakhimov <d.rakhimov@xsolla.com>
2023-07-14 13:32:01 +0800

    Fix onCancel event is invoked even on successful social auth

--- Assets/Xsolla/Auth/Internal/StandaloneSocialAuth.cs
+++ Assets/Xsolla/Auth/Internal/StandaloneSocialAuth.cs
@@ -5,24 +5,30 @@ namespace Xsolla.Auth
 {
 	internal class StandaloneSocialAuth
 	{
+		private bool isBrowserClosedByCode;
+
 		public void Perform(SocialProvider provider, Action onSuccess, Action<Error> onError, Action onCancel)
 		{
 			var socialNetworkAuthUrl = XsollaAuth.GetSocialNetworkAuthUrl(provider);
 			var browser = XsollaWebBrowser.InAppBrowser;
 			browser.Open(socialNetworkAuthUrl);
 
-			browser.AddCloseHandler(() => onCancel?.Invoke());
+			browser.AddCloseHandler(() => {
+				if (!isBrowserClosedByCode)
+					onCancel?.Invoke();
+			});
+
 			browser.AddUrlChangeHandler(url => UrlChangedHandler(url, onSuccess, onError));
 		}
 
-		private static void UrlChangedHandler(string url, Action onSuccess, Action<Error> onError)
+		private void UrlChangedHandler(string url, Action onSuccess, Action<Error> onError)
 		{
 			if (ParseUtils.TryGetValueFromUrl(url, ParseParameter.code, out var code))
 			{
 				XsollaAuth.ExchangeCodeToToken(
 					code,
-					() =>
-					{
+					() => {
+						isBrowserClosedByCode = true;
 						XsollaWebBrowser.Close();
 						onSuccess?.Invoke();
 					},
