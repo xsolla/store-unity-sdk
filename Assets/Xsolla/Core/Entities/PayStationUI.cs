c2e421099654fd278ec9b2ee71d53394b2a84da3
Olga Salina <o.salina@xsolla.com>
2024-04-10 15:10:34 +0300

    SDK-3574 Add mobile and desktop parameters for pay station ui settings

--- Assets/Xsolla/Core/Entities/PayStationUI.cs
+++ Assets/Xsolla/Core/Entities/PayStationUI.cs
@@ -9,5 +9,43 @@ namespace Xsolla.Core
 		public string theme;
 		public string version;
 		public bool is_independent_windows;
+		public Desktop desktop;
+		public Mobile mobile;
+
+		[Serializable]
+		public class Desktop
+		{
+			public Header header;
+
+			[Serializable]
+			public class Header
+			{
+				public bool? is_visible;
+				public bool? visible_logo;
+				public bool? visible_name;
+				public string type;
+				public bool? close_button;
+			}
+		}
+
+		[Serializable]
+		public class Mobile
+		{
+			public string mode;
+			public Footer footer;
+			public Header header;
+
+			[Serializable]
+			public class Footer
+			{
+				public bool? is_visible;
+			}
+
+			[Serializable]
+			public class Header
+			{
+				public bool? close_button;
+			}
+		}
 	}
 }
