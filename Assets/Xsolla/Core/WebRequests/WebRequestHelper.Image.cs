31c0f4b9767307a3f38646933c8bfe8ebbef4b4a
Denis Rakhimov <d.rakhimov@xsolla.com>
2025-01-17 14:00:39 +0800

    SDK-4167 Fix memory leaks related to webrequests

--- Assets/Xsolla/Core/WebRequests/WebRequestHelper.Image.cs
+++ Assets/Xsolla/Core/WebRequests/WebRequestHelper.Image.cs
@@ -14,8 +14,7 @@ namespace Xsolla.Core
 
 		private IEnumerator PerformImage(string url, Action<Sprite> onComplete, Action<Error> onError)
 		{
-			yield return PerformImage(url, texture =>
-			{
+			yield return PerformImage(url, texture => {
 				var spriteRect = new Rect(0, 0, texture.width, texture.height);
 				var pivot = new Vector2(0.5f, 0.5f);
 				var sprite = Sprite.Create(texture, spriteRect, pivot);
@@ -25,8 +24,10 @@ namespace Xsolla.Core
 
 		private IEnumerator PerformImage(string url, Action<Texture2D> onComplete, Action<Error> onError)
 		{
-			var webRequest = UnityWebRequestTexture.GetTexture(url);
-			yield return StartCoroutine(PerformWebRequest(webRequest, onComplete, onError));
+			using (var webRequest = UnityWebRequestTexture.GetTexture(url))
+			{
+				yield return StartCoroutine(PerformWebRequest(webRequest, onComplete, onError));
+			}
 		}
 	}
 }
