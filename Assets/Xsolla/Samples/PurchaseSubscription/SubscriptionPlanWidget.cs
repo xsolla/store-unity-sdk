6b4d1f8f7739d12a1726e37299545725cdb132e6
Denis Rakhimov <d.rakhimov@xsolla.com>
2026-03-06 12:33:26 +0800

    SDK-4162 Add samples for subscriptions

---
+++ Assets/Xsolla/Samples/PurchaseSubscription/SubscriptionPlanWidget.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Xsolla.Samples.PurchaseSubscription
+{
+	public class SubscriptionPlanWidget : MonoBehaviour
+	{
+		// Declaration of variables for UI elements
+		public Text NameText;
+		public Text DescriptionText;
+		public Text PriceText;
+		public Text PeriodText;
+		public Button BuyButton;
+	}
+}
